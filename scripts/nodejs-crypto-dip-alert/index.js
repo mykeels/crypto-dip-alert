714baa63bb2b025a38beb4d66d0be7100a413998
Ikechi Michael <mykehell123@gmail.com>
2020-04-24 13:10:48 +0100

    feat: add rough draft

---
+++ scripts/nodejs-crypto-dip-alert/index.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+require('isomorphic-fetch');
+const { program } = require('commander');
+const chalk = require('chalk');
+
+const INTERVAL_MS = 10000;
+const PAGINATION_COUNT = 4;
+const COINCAP_ASSETS_URL = 'https://api.coincap.io/v2/assets';
+
+program
+  .option('-c, --coins <coins>', 'A comma-separated list of coins to track', value => value.split(',').map(c => c.trim()), ['BTC', 'ETH'])
+  .option('-p, --price <price>', 'A price value to monitor', '0.01')
+  .option('-t, --type <type>', '"percent" or "cents"', 'percent')
+  .option('-l, --list', 'List available Cryptos')
+  .parse(process.argv);
+
+(async () => {
+  const { data: assets } = await fetch(COINCAP_ASSETS_URL).then(res => res.json());
+  const assetDict = assets.reduce((obj, item) => ({
+    ...obj,
+    [item.symbol]: item
+  }), {});
+
+  if (program.list) {
+    console.log(
+      assets
+        .map(({ symbol }) => symbol)
+        .reduce((arr, item) => {
+          if (!arr[arr.length - 1] || arr[arr.length - 1].length === PAGINATION_COUNT) {
+            arr.push([item])
+          }
+          else {
+            arr[arr.length - 1].push(item)
+          }
+          return arr;
+        }, [])
+        .map(arr => arr.join('\t'))
+        .join('\n')
+    );
+  }
+  else {
+    const known = program.coins.filter(c => assetDict[c]);
+    const unknown = program.coins.filter(c => !assetDict[c]);
+
+    if (unknown.length) {
+      console.log(`Unknown: ${unknown.join(', ')}`);
+    }
+    if (!known.length) return;
+
+    const assetsArgs = known.map(c => assetDict[c].id);
+
+    console.log(`
+Monitoring: ${known.join(', ')}
+
+Price Dip: ${program.price}
+
+Dip Type: ${program.type}
+`);
+
+    let maxPriceSinceLastDip = {};
+
+    function dipThresholdInCents(maxPrice) {
+      return program.type === 'percent' ?
+        (Number(program.price) / 100 * maxPrice) :
+        (program.price / 100);
+    }
+
+    async function sleep(ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+      })
+    }
+
+    async function checkPrices() {
+
+      const cryptos = await Promise.all(
+        known.map(symbol => {
+          const id = assetDict[symbol].id;
+          return fetch(`https://api.coincap.io/v2/assets/${id}`).then(res => res.json());
+        })
+      );
+
+      for (let { data: coin } of cryptos) {
+        if (!maxPriceSinceLastDip[coin.symbol]) {
+          maxPriceSinceLastDip[coin.symbol] = +Number(coin.priceUsd).toFixed(2);
+        }
+
+        const maxPrice = maxPriceSinceLastDip[coin.symbol];
+
+        const currentPrice = +Number(coin.priceUsd).toFixed(2);
+
+        const dipThreshold = dipThresholdInCents(maxPrice);
+
+        const priceDifference = +Number(currentPrice - maxPrice).toFixed(2);
+
+        console.log(
+          `${coin.symbol}: $${currentPrice}`,
+          (priceDifference > 0) ? chalk.green(`+$${priceDifference}`) : (
+            (priceDifference < 0) ? chalk.red(`-$${Math.abs(priceDifference)}`) : ''
+          )
+        );
+
+        if (currentPrice > maxPrice) {
+          maxPriceSinceLastDip[coin.symbol] = currentPrice;
+        }
+        else if ((maxPrice - currentPrice) >= dipThreshold) {
+          // TODO send notification
+          console.log(coin.symbol, 'SEND NOTIFICATION');
+        }
+
+      }
+
+      await sleep(INTERVAL_MS);
+      await checkPrices();
+    };
+
+    await checkPrices();
+  }
+})();
