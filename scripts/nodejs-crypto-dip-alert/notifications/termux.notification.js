a2c976148db34e56a5dae811a166871d0dbc70a1
Adekoreday <khord4eng@gmail.com>
2020-05-10 20:45:48 +0100

    update package

--- scripts/nodejs-crypto-dip-alert/notifications/termux.notification.js
+++ scripts/nodejs-crypto-dip-alert/notifications/termux.notification.js
@@ -5,7 +5,6 @@ const notify = (coin, maxPrice, currentPrice, dipThreshold) => {
    const dip = `-$${Math.abs(currentPrice - maxPrice)}`;
    const text = `We detected a ${dip} dip in the price of ${coin.symbol}, and thought we should let you know.\n\nThe Price dropped from $${maxPrice} to $${currentPrice}.`;
 
-   console.log('attempt notifcation');
     if (api.hasTermux){
     api.vibrate()
        .duration(1000)
