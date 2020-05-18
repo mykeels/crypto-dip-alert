486d613bd5f8c9f89d1825b42b5ce640023e9650
mykeels <mykehell123@gmail.com>
2020-05-18 18:38:25 +0100

    fix: add flag for termux

--- scripts/nodejs-crypto-dip-alert/notifications/termux.notification.js
+++ scripts/nodejs-crypto-dip-alert/notifications/termux.notification.js
@@ -1,21 +1,23 @@
 const api = require('termux')
- 
+
 const notify = (coin, maxPrice, currentPrice, dipThreshold) => {
+
+   if (!process.env.NOTIFY_TERMUX || process.env.NOTIFY_TERMUX !== 'true') return;
 
    const dip = `-$${Math.abs(currentPrice - maxPrice)}`;
    const text = `We detected a ${dip} dip in the price of ${coin.symbol}, and thought we should let you know.\n\nThe Price dropped from $${maxPrice} to $${currentPrice}.`;
 
-    if (api.hasTermux){
-    api.vibrate()
-       .duration(1000)
-       .run()
-     
-    api.notification()
-        .id(1)
-        .title('CRYPTO DIP')
-        .content(text)
-   .run()
-    }
+   if (api.hasTermux) {
+      api.vibrate()
+         .duration(1000)
+         .run()
+
+      api.notification()
+         .id(1)
+         .title('Crypto Dip Alert')
+         .content(text)
+         .run()
+   }
 }
 
 module.exports = notify;
